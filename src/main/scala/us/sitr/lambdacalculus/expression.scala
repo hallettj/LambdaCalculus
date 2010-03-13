f678841202c276507db043b6ea3169fb6d4f6f10
Jesse Hallett <hallettj@gmail.com>
2010-03-13 15:05:28 -0800

    Implemented beta and alpha conversions.

--- src/main/scala/us/sitr/lambdacalculus/expression.scala
+++ src/main/scala/us/sitr/lambdacalculus/expression.scala
@@ -4,21 +4,67 @@ import scala.util.parsing.combinator._
 
 sealed abstract class Expression {
   def substitute(orig: Var, sub: Expression): Expression
+  def freeVars: Set[Var]
+  def boundVars: Set[Var]
   def toString: String
+
+  def alphaConversion(conflicting: Set[Var]): Expression = this match {
+    case Var(_) => this
+    case Function(arg, body) =>
+      if (conflicting contains arg) {
+        val newArg = arg.prime(conflicting ++ this.boundVars)
+        Function(newArg, body.substitute(arg, newArg))
+      } else
+        Function(arg, body alphaConversion conflicting)
+    case Application(a, b) =>
+      Application(a alphaConversion conflicting,
+                  b alphaConversion conflicting)
+  }
+
+  def betaConversion: Expression = this match {
+    case Application(Function(arg, body), b) => body.substitute(arg, b)
+    case Application(a, b) => Application(a.betaConversion, b)
+    case _ => this
+  }
+
+  //def etaConversion: Expression
 }
 
 case class Var(name: String) extends Expression {
-  def substitute(orig: Var, sub: Expression): Expression = this
+  def substitute(orig: Var, sub: Expression): Expression =
+    if (orig == this) sub else this
+
+  def freeVars: Set[Var] = Set(this)
+  def boundVars: Set[Var] = Set()
+
   override def toString: String = name
+
+  /**
+   * Adds prime (') to the variable name as many times as is necessary to get a
+   * variable name that does not conflict with anything in `conflicting`.
+   */
+  private[lambdacalculus] def prime(conflicting: Set[Var]): Var = {
+    val newVar = Var(name + "'")
+    if (conflicting contains newVar)
+      newVar.prime(conflicting)
+    else
+      newVar
+  }
 }
 
 case class Function(argument: Var, body: Expression) extends Expression {
   def substitute(orig: Var, sub: Expression): Expression = {
-    if (orig != argument)
-      Function(argument, body.substitute(orig, sub))
-    else
+    if (orig != argument) {
+      if ((sub.freeVars) contains argument)
+        (this alphaConversion sub.freeVars).substitute(orig, sub)
+      else
+        Function(argument, body.substitute(orig, sub))
+    } else
       this
   }
+
+  def freeVars: Set[Var] = body.freeVars - argument
+  def boundVars: Set[Var] = body.boundVars + argument
 
   override def toString: String = String.format("λ%s.%s", argument, body)
 }
@@ -27,14 +73,17 @@ case class Application(function: Expression, argument: Expression) extends Expre
   def substitute(orig: Var, sub: Expression): Expression =
     Application(function.substitute(orig, sub), argument.substitute(orig, sub))
 
+  def freeVars: Set[Var] = function.freeVars ++ argument.freeVars
+  def boundVars: Set[Var] = function.boundVars ++ argument.boundVars
+
   override def toString: String = {
     val left = function match {
       case Function(_, _) => "("+ function +")"
-      case a => a
+      case _ => function
     }
     val right = argument match {
       case Var(_) => argument
-      case b => "("+ b +")"
+      case _ => "("+ argument +")"
     }
     left +" "+ right
   }
@@ -48,7 +97,7 @@ class LambdaParsers extends RegexParsers {
 
   def simpleExpression: Parser[Expression] = (
       function
-    | identifier
+    | variable
     | constant
     | "("~>expression<~")"
   )
@@ -63,13 +112,13 @@ class LambdaParsers extends RegexParsers {
       case exp~exps => (exp /: exps) { (app, e) => Application(app, e) }
     }
 
-  def arguments: Parser[List[Var]] = rep1(identifier)
+  def arguments: Parser[List[Var]] = rep1(variable)
 
   def lambda: Parser[String] = """\\|λ""".r
 
-  def identifier: Parser[Var] = """[a-z]""".r ^^ { Var(_) }
+  def variable: Parser[Var] = """[a-z]'*""".r ^^ { Var(_) }
 
-  def constant: Parser[Var] = """[A-Z0-9+\-*/]+""".r ^^ { Var(_) }
+  def constant: Parser[Var] = """[^a-z\\λ\(\)\s\.']+""".r ^^ { Var(_) }
 }
 
 object Expression extends LambdaParsers {
@@ -77,10 +126,5 @@ object Expression extends LambdaParsers {
     (parseAll(expression, input): @unchecked) match {
       case Success(e, _) => e
     }
-  }
-
-  def betaReduction(exp: Expression): Expression = exp match {
-    case Application(Function(arg, body), b) => body.substitute(arg, b)
-    case _ => exp
   }
 }
